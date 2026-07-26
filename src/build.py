a760865aa6ed3e019c867fb45eb6f5a6f46f3e00
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2026-07-26 11:50:38 +0200

    oops

--- src/build.py
+++ src/build.py
@@ -214,7 +214,7 @@ class Build:
         for file in Path(self.config.project_dir).glob(glob):
             if not file.is_file():
                 continue
-            files.append(file)
+            files.append(str(file))
 
         self.mod_json["api"] = { "include": files }
 
